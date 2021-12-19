799c5fe35b9bf79a72162bd7b519cfb740de5350
Ruth G. N <32371319+ruthgn@users.noreply.github.com>
2021-12-18 22:36:20 -0500

    Update app.py
    Edit bottom caption

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ if is_for_prediction:
 
 
 st.write('---')  
-st.caption("Ran into a bug or saw something that could be improved on the app? Submit a [Github issue](https://github.com/ruthgn/AutomatEDA/issues) or [fork the repository](https://github.com/forks/ruthgn/Beer-Recommender) to create a pull request.")
+st.caption("Ran into a bug or saw something on the app that needs to be improved? Submit a [Github issue](https://github.com/ruthgn/AutomatEDA/issues) or [fork the repository](https://github.com/ruthgn/AutomatEDA) to create a pull request.")
